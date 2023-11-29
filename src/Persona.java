27a33fbfa8aae78fad2efa5e814ee6e65edd70cc
OscarLuque-31 <a_oscar.luque.hidalgo@iespablopicasso.es>
2023-11-29 09:49:04 +0100

    Clase Persona

---
+++ src/Persona.java
@@ -0,0 +1,21 @@
+
+public class Persona {
+  
+  public String nombre;
+
+  public String apellidos;
+  
+  public int edad;
+  
+  public Persona(String nombre,String apellidos,int edad) {
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.edad = edad;
+  }
+  
+  public int Cumpleaños() {
+    edad += 1;
+    return edad;
+  }
+
+}
